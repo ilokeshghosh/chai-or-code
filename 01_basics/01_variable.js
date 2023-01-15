1fa3becceedad72129d85889fadca43db622c10a
Lokesh Ghosh <lghosh857@gmail.com>
2023-01-15 13:44:29 +0000

    learned variable in js

---
+++ 01_basics/01_variable.js
@@ -0,0 +1,9 @@
+const id = 123454;
+let email ="ghoshlokesh57@gmail.com"
+var city = "Kolkata"
+state = "West Bengal"
+let country;
+
+console.log(id);
+
+console.table([id, email, city, state, country])
