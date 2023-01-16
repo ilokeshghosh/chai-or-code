2f8dec1fde8ce8edf3064cf36622528ac6899095
Lokesh Ghosh <lghosh857@gmail.com>
2023-01-16 14:02:58 +0000

    .

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,24 @@
+"use strict" //treat all js as newer version
+
+// alert(4+8); //can't use in newer version
+
+console.log("Lokesh Ghosh");
+
+let name="Lokesh Ghosh"
+let age=22;
+let isLoggedIn = false
+
+
+//number=>2 to power 53
+//bigint 
+//string => ""/''
+//boolean => true/false
+//null =>standalone value
+//undefined =>
+//symbol=> unique
+
+
+//object
+
+console.log(typeof null)   //type object
+console.log(typeof undefined)  //type undefined
