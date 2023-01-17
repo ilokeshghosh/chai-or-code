49a67f6f4571f28b9008e4acb587601f6de0d7ef
Lokesh Ghosh <lghosh857@gmail.com>
2023-01-17 04:52:03 +0000

    .

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,14 @@
+let score =55;
+let score2= "55abc";
+let score3= null;
+let score4 = undefined;
+let score5 = true;
+// let score6=
+
+let IsScoreNumber = Number(score);
+console.log(typeof(IsScoreNumber))
+console.log(IsScoreNumber);
+
+IsScoreNumber = Number(score2);
+console.log(typeof(IsScoreNumber))
+console.log(IsScoreNumber);
