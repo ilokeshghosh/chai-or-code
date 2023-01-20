34aaf51ec2f0e3b95851b08aa72e852d3a02caad
Lokesh Ghosh <lghosh857@gmail.com>
2023-01-20 13:28:23 +0000

    .

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -1,3 +1,4 @@
+/*
 let score =55;
 let score2= "55abc";
 let score3= null;
@@ -35,6 +36,7 @@ console.log(IsScoreNumber);
 //"33abc" => NaN //NaN is number type
 //true => 1; false => 0;
 
+ 
 let isLoggedIn = 1
 let booleanIsLoggedIn = Boolean(isLoggedIn);
 console.log(booleanIsLoggedIn);
@@ -48,11 +50,51 @@ console.log(booleanIsLoggedIn);
 // 1 => true ; 0 => false
 // "" => false
 // "lokesh" => true
-
+ 
 let someNumber = true
 
 let stringNumber = String(someNumber);
 console.log(stringNumber)
 console.log(typeof(stringNumber))
 
+*/
 
+// *********************** Operations ***********************
+
+let value = 3
+let negValue = -value
+// console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3);
+// console.log(2/3);
+// console.log(2%3);
+
+let str1 = "hello"
+let str2 = " hitesh"
+
+let str3 = str1 + str2
+// console.log(str3);
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2");
+
+// console.log( (3 + 4) * 5 % 3);
+
+// console.log(+true);
+// console.log(+"");
+
+let num1, num2, num3
+
+num1 = num2 = num3 = 2 + 2
+
+let gameCounter = 100
+++gameCounter;
+console.log(gameCounter);
+
+// link to study
+// https://tc39.es/ecma262/multipage/abstract-operations.html#sec-type-conversion
