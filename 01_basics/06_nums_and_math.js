02fe9f119b2bd9e82e985dd600cc949268685694
Lokesh Ghosh <lghosh857@gmail.com>
2023-08-10 07:46:49 +0000

    number and math

---
+++ 01_basics/06_nums_and_math.js
@@ -0,0 +1,37 @@
+let num =23;
+// console.log(num);
+let score = new Number(44.545654);
+// console.log(score);
+
+// console.log(typeof(score.toString()));
+// console.log(score.toFixed(3))
+// console.log(score.toPrecision(2));
+let hundreds = 10000000;
+// console.log(hundreds.toLocaleString("en-IN"));
+
+//*****************************Maths***************************/
+// console.log(Math.PI)
+// console.log(Math);
+
+// console.log(Math.abs(-1));
+// console.log(Math.round(9.5));
+// console.log(Math.ceil(4.1));
+// console.log(Math.floor(4.9));
+// console.log(Math.min(2,4,0,1));
+// console.log(Math.max(2,4,0,1));
+// console.log(Math.pow(2,3));
+
+// console.log(Math.random());
+// console.log(Math.random()*10);
+// console.log((Math.random()*10)+1);
+// console.log(Math.floor((Math.random()*10)+1));
+
+let min =10;
+let max=20;
+
+console.log(Math.floor((Math.random()*(max-min + 1))+min));
+
+
+
+
+
