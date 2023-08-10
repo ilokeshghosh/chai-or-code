0f9444bccbd0248ade7a440a72c63c178bd74366
Lokesh Ghosh <lghosh857@gmail.com>
2023-08-10 05:11:56 +0000

    revision old and learn memory

--- 01_basics/04_comparision.js
+++ 01_basics/04_comparision.js
@@ -1,17 +1,18 @@
 //console.log(2==1);
 //console.log(2!=1);
 
-console.log("2">1);
-console.log("02">1);
+// console.log("2">1);
+// console.log("02">1);
 
-console.log(null > 0);
-console.log(null == 0);
-console.log(null>=0); // >= and == works differently
+// console.log(null > 0);
+// console.log(null == 0);
+// console.log(null>=0); // >=(comparison) and ==(equality check) works differently , comparison convert null to a number i.e 0.
 
-console.log(undefined==0);
-console.log(undefined>0);
-console.log(undefined<0);
+// console.log(undefined==0);
+// console.log(undefined>0);
+// console.log(undefined<0);
 
 
 //=== strict check
 console.log("2"===2);
+console.log("2a"==2)
--- 01_basics/datatypes-summary.js
+++ 01_basics/datatypes-summary.js
@@ -1,20 +1,20 @@
 //  Primitive
 
-//  7 types : String, Number, Boolearn, null, undefined, Symbol, BigInt
+//  7 types : String, Number, Boolean, null, undefined, Symbol, BigInt
 
 const score = 100
 const scoreValue = 100.3
 
 const isLoggedIn = false
-const outsideTemp = null
-let userEmail;
+const outsideTemp = null //object type
+let userEmail; //undefined type
 
-const id = Symbol('123')
+const id = Symbol('123')  //symbol type
 const anotherId = Symbol('123')
 
-console.log(id === anotherId);
+// console.log(id === anotherId);
 
-// const bigNumber = 3456543576654356754n
+const bigNumber = 3456543576654356754n  //bitint type
 
 
 
@@ -22,16 +22,51 @@ console.log(id === anotherId);
 
 // Array, Objects, Functions
 
-const heros = ["shaktiman", "naagraj", "doga"];
-let myObj = {
+const heros = ["shaktiman", "naagraj", "doga"]; //object type
+let myObj = {   //object type
     name: "hitesh",
     age: 22,
 }
 
-const myFunction = function(){
+const myFunction = function(){  //object function type
     console.log("Hello world");
 }
+// myFunction.call();
 
-console.log(typeof anotherId);
+// console.log(typeof anotherId);
+// console.log(typeof bigNumber);
+
+// console.log(typeof heros);
+// console.log(typeof myObj);
+// console.log(typeof myFunction);
 
 // https://262.ecma-international.org/5.1/#sec-11.4.3
+
+
+
+//***************Memory*******************/
+
+// Stack(primitive datatypes) & Heap(non-primitive datatypes)
+
+let name1= "lokesh";
+let name2= name1;
+// console.log(name1);
+// console.log(name2);
+name2="ghosh";
+// console.log(name1);
+// console.log(name2);
+
+let myAccount1 = {
+    userId:"lokesh@gmail.com",
+    pass:"lokesh123",
+}
+
+let myAccount2 = myAccount1;
+
+console.log(myAccount1.userId);
+console.log(myAccount2.userId);
+
+myAccount2.userId="ghosh@gmail.com";
+
+console.log(myAccount1.userId);
+console.log(myAccount2.userId);
