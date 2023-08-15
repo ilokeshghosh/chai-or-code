ea55c012de13d50b2b99f464387e6de69506e449
Lokesh Ghosh <lghosh857@gmail.com>
2023-08-15 07:20:57 +0000

    scope level and mini hoisting

--- 03_basics/02_scopes.js
+++ 03_basics/02_scopes.js
@@ -7,6 +7,45 @@ if(true){
     const c=500;
 }
 
-console.log(a);
-console.log(b);
-console.log(c);
+// console.log(a);
+// console.log(b);
+// console.log(c);
+
+
+function one(){
+    const username="lokesh"
+    function two(){
+        const website = "github.com"
+        console.log(username);
+    }
+    // console.log(website);
+    two();
+}
+// one();
+
+
+if(true){
+    const username = "Lokesh"
+    if(username == "Lokesh"){
+        const website =" youtube"
+        // console.log(username+website);
+    }
+    // console.log(website);
+
+}
+
+// console.log(username);
+
+
+// ****************interesting**********************
+addone(5);
+function addone(num){
+    return num+1;
+}
+
+addTwo(4)
+const addTwo = function(num){
+    return num+2;
+}
+
+ 
