169ece05bc6c962419c2b9c523fa539988a2f336
Lokesh Ghosh <lghosh857@gmail.com>
2023-08-20 16:15:38 +0000

    for, while, do-while loop

---
+++ 05_iterations/one.js
@@ -0,0 +1,8 @@
+// for loop
+for(let i=0;i<=10;i++){
+    if(i==5){
+        // break;
+        continue;
+    }
+    console.log(i);
+}
---
+++ 05_iterations/two.js
@@ -0,0 +1,12 @@
+// while
+let i=0;
+while (i<=10) {
+    // console.log(i);
+    i++;
+}
+
+// do-while
+let n=11
+do{
+    console.log(n);
+}while(n<=10);
