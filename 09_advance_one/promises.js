2350d82104643df4fd04b16b1715e3cecf110ec5
Lokesh Ghosh <lghosh857@gmail.com>
2023-09-01 03:59:46 +0000

    promise 3

--- 09_advance_one/promises.js
+++ 09_advance_one/promises.js
@@ -36,46 +36,71 @@
 // })
 
 
-const promiseFour = new Promise(function(resolve, reject){
+// const promiseFour = new Promise(function(resolve, reject){
 
-    setTimeout(()=>{
-        let error = true;
-        if(!error){
-            resolve({username:"lokesh", password:"123"});
-        }else{
-            reject('ERROR Something went wrong')
-        }
-    },2000)
+//     setTimeout(()=>{
+//         let error = true;
+//         if(!error){
+//             resolve({username:"lokesh", password:"123"});
+//         }else{
+//             reject('ERROR Something went wrong')
+//         }
+//     },2000)
+// })
+
+// promiseFour.then((user)=>{
+//     console.log(user);
+//     return user.username;
+// }).then((username)=>{
+//     console.log(username);
+// }).catch(function(error){
+//     console.log(error);
+// }).finally(()=> console.log("The promise is either resolved or rejected "))
+
+
+// const promiseFive = new Promise((resolve, reject)=>{
+//     setTimeout(()=>{
+//         let error = true;
+//         if(!error){
+//             resolve({username:"js", password:"123"});
+//         }else{
+//             reject('ERROR : js went wrong')
+//         }
+//     },2000)
+// });
+
+// async function consumePromiseFive(){
+//    try{
+//     const response = await promiseFive
+//     console.log(response);
+//    }catch(error){
+//     console.log(error);
+//    }
+// }
+
+// consumePromiseFive();
+
+// async function getdata(){
+//     try{
+//         const response = await fetch('https://api.github.com/users/ilokeshghosh');
+//         const data = await response.json();
+//         console.log(data);
+//     }catch(error){
+//         console.log("E: ",error);
+//     }
+// }
+
+// getdata();
+
+
+
+fetch('https://api.github.com/users/ilokeshghosh')
+.then((respose)=>{
+    // console.log(respose.login);
+    return respose.json();
+}).then((data)=>{
+    console.log(data.login)
 })
-
-promiseFour.then((user)=>{
-    console.log(user);
-    return user.username;
-}).then((username)=>{
-    console.log(username);
-}).catch(function(error){
-    console.log(error);
-}).finally(()=> console.log("The promise is either resolved or rejected "))
-
-
-const promiseFive = new Promise((resolve, reject)=>{
-    setTimeout(()=>{
-        let error = true;
-        if(!error){
-            resolve({username:"js", password:"123"});
-        }else{
-            reject('ERROR : js went wrong')
-        }
-    },2000)
-});
-
-async function consumePromiseFive(){
-   try{
-    const response = await promiseFive
-    console.log(response);
-   }catch(error){
-    console.log(error);
-   }
-}
-
-consumePromiseFive();
+.catch((error)=>{
+    console.log("Error");
+})
