b939fe12a91f682045bc1605bad617b8f786e3c7
Lokesh Ghosh <lghosh857@gmail.com>
2023-09-03 15:14:02 +0000

    oops 2

---
+++ 10_classes_and_oop/oop.js
@@ -0,0 +1,43 @@
+const user ={
+    username:"lokesh",
+    loginCount : 5,
+    signedIn: true,
+
+    getUserDetails:function(){
+        // console.log('Got User Details')
+        // console.log(`Username : ${this.username}`);
+        console.log(this);
+    }
+}
+
+// console.log(user.getUserDetails());
+// console.log(this); //output = {}
+// console.log(globalThis)
+
+// const promiseOne = new Promise()
+
+function User(username, loginCount, isLoggedIn){
+    this.username= username;
+    this.loginCount = loginCount;
+    this.isLoggedIn = isLoggedIn;
+
+    this.greeting = function(){
+        console.log(`Welcome ${this.username}`)
+    }
+    // return this;
+
+}
+
+
+const userOne = new User("lokesh", 12, true)
+const userTwo = new User("ghosh", 20, false);
+// console.log(userOne);
+console.log(userOne.greeting());                 
+console.log(userOne.constructor);                 
+// console.log(userTwo);
+
+
+
+// step 1 = create new object
+// step 2 = constructor function is called
+// step 3 = all arguments are injected in this 
