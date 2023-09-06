6d56efdbc205c9a9bac3e361a1cd9cc0c8ec23fd
Lokesh Ghosh <lghosh857@gmail.com>
2023-09-06 05:15:21 +0000

    advance object : getOwnPropertyDescriptor() & defineProperty()

---
+++ 10_classes_and_oop/mathpi.js
@@ -0,0 +1,47 @@
+// console.log(Math.PI);
+
+const descriptor=Object.getOwnPropertyDescriptor(Math, 'PI');
+// console.log(descriptor);
+
+
+const chai ={
+    name:'milk tea',
+    price: 250,
+    isAvailable: true,
+
+    isChaiReady : function readyChai(){
+        console.log("not ready");
+    }
+}
+
+// chai.name = "lokesh";
+// console.log(chai);
+
+
+// console.log(Object.getOwnPropertyDescriptor(chai, 'name'));
+
+// this will not change the property
+// console.log(Object.getOwnPropertyDescriptor(chai, 'name').configurable);
+// Object.getOwnPropertyDescriptor(chai, 'name').configurable= false;
+// console.log(Object.getOwnPropertyDescriptor(chai, 'name').configurable);
+
+
+
+Object.defineProperty(chai, 'name', {
+    // writable: false,
+    enumerable: false
+})
+
+
+console.log(Object.getOwnPropertyDescriptor(chai, 'name'));
+
+
+// this will not work as writable is false
+// chai.name = "lokesh";
+// console.log(chai);
+
+for(let [key, value] of Object.entries(chai)){
+    if(typeof value !== 'function'){
+        console.log(`${key} : ${value}`);
+    }
+}
