3a2c2aeb6d6b874f158e068122fb227aa5bb9793
Lokesh Ghosh <lghosh857@gmail.com>
2023-09-09 13:17:55 +0530

    wrap-up : 04_control_flow

--- 04_control_flow/truthy.js
+++ 04_control_flow/truthy.js
@@ -39,10 +39,10 @@ const emptyObj = {}
 
 let val1;
 // val1 = 5??10;
-// val1 = null ?? 10;
+val1 = null ?? 10;
 // val1 = undefined ?? 15;
-val1 = undefined ?? 15 ?? 20;
-// console.log(val1);
+// val1 = undefined ?? 15 ?? 20;
+console.log(val1);
 
 
 // terniary operator
